794e9138898e29fd33484d6e3680ebf64a001005
Metehan Dal <metehan654101@gmail.com>
2022-08-01 11:17:10 +0300

    Uploaded

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  categories = [];
+  highlights = [];
+  featured = [];
 
-  constructor() {}
+  catSlideOpts = {
+    slidesPerView: 3.5,
+    slidesOffsetBefore: 11,
+    spaceBetween: 10,
+    freeMode: true,
+
+  };
+
+  highlightSlideOpts = {
+    slidesPerView: 1.05,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true
+  };
+
+  featuredSlideOpts = {
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    freeMode: true
+  };
+
+  showLocationDetail = false;
+
+
+  constructor(private http: HttpClient) {}
+
+  
+  ngOnInit(): void {
+      this.http.get('https://devdactic.fra1.digitaloceanspaces.com/foodui/home.json').subscribe((res: any) => {
+        console.log(res);
+        this.categories = res.categories;
+        this.highlights = res.highlights;
+        this.featured = res.featured;
+      });
+  }
+
+  doRefresh(ev) {
+    setTimeout(() => {
+      ev.target.complete();
+    }, 2000)
+  }
+
+  onScroll(ev){
+    const offset = ev.detail.scrollTop;
+    this.showLocationDetail = offset > 50;
+  }
+
 
 }
